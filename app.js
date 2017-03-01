edcc2cc956d69e4a48391cf3aa658f3864cff79c
JimGreenberg <greenjberg@gmail.com>
2017-03-01 16:22:14 -0500

    proper mouse movement

--- app.js
+++ app.js
@@ -1,106 +1,65 @@
-// document.onkeydown = e => {
-//     if (e.keyCode === 68){//d
-//     player.rt = true;
-//     }
-//     else if (e.keyCode === 83){//s
-//       player.dn = true;
-//     }
-//     else if (e.keyCode === 65){//a
-//       player.lf = true;
-//     }
-//     else if (e.keyCode === 87){//w
-//       player.up = true;
-//     }
-//   };
-//
-// document.onkeyup = e => {
-//     if (e.keyCode === 68){//d
-//       player.rt = false;
-//     }
-//     else if (e.keyCode === 83){//s
-//       player.dn = false;
-//     }
-//     else if (e.keyCode === 65){//a
-//       player.lf = false;
-//     }
-//     else if (e.keyCode === 87){//w
-//       player.up = false;
-//     }
-//   };
-
-
+const WIDTH = 1000;
+const HEIGHT = 600;
 class Player {
   constructor(id) {
-    this.id = id;
-    this.x = 250;
-    this.y = 250;
-    this.icon = 1;
-    this.rt = false;
-    this.dn = false;
-    this.up = false;
-    this.lf = false;
-    this.accel = 0.5;
+    this.x = Math.random() * WIDTH;
+    this.y = Math.random() * HEIGHT;
+    arena.x = WIDTH / 2 - this.x;
+    arena.y = HEIGHT / 2 - this.y;
     this.vx = 0;
     this.vy = 0;
-    this.theta = 0;
   }
 
   updatePostition() {
-    let newVX = stage.mouseX - this.x;
-    let newVY = stage.mouseY - this.y;
-    let newV = Math.sqrt(Math.pow(newVX, 2) + Math.pow(newVY, 2))
-    this.vx += newVX / newV;
-    this.vy += newVY / newV;
-    if (newV < 0.2 || !stage.mouseInBounds) {
+    let newVX = stage.mouseX - 500;
+    let newVY = stage.mouseY - 300;
+    let newV = Math.sqrt(Math.pow(newVX, 2) + Math.pow(newVY, 2));
+    if (newV === 0) {return;}
+    this.vx = newVX / newV;
+    this.vy = newVY / newV;
+    this.vx *= 3;
+    this.vy *= 3;
+
+    if (this.x > WIDTH - 25) {
+      this.x = WIDTH - 25;
+      arena.x = -WIDTH / 2 + 25;
       this.vx = 0;
+    }
+    if (this.y > HEIGHT - 25) {
+      this.y = HEIGHT - 25;
+      arena.y = -HEIGHT / 2 + 25;
       this.vy = 0;
-    }
-
-  //   if (this.rt) {
-  //     this.vx += this.accel;
-  //   }
-  //   if (this.lf) {
-  //     this.vx -= this.accel;
-  //   }
-  //   if (this.up) {
-  //     this.vy -= this.accel;
-  //   }
-  //   if (this.dn) {
-  //     this.vy += this.accel;
-  //   }
-    this.x += this.vx;
-    this.y += this.vy;
-    this.vx = this.vx * 0.8;
-    this.vy = this.vy * 0.8;
-
-    if (this.x > 975) {
-      this.x = 975;
-      this.vx = -this.vx * 0.5;
-      this.icon++;
-    }
-    if (this.y > 575) {
-      this.y = 575;
-      this.vy = -this.vy * 0.5;
-      this.icon++;
 
     }
     if (this.x < 25) {
       this.x = 25;
-      this.vx = -this.vx * 0.5;
-      this.icon++;
-
+      arena.x = WIDTH / 2 - 25;
+      this.vx = 0;
     }
     if (this.y < 25) {
       this.y = 25;
-      this.vy = -this.vy * 0.5;
-      this.icon++;
+      arena.y = HEIGHT / 2 - 25;
+      this.vy = 0;
+    }
 
+
+    if (stage.mouseInBounds) {
+      this.x += this.vx;
+      this.y += this.vy;
+      arena.x -= this.vx;
+      arena.y -= this.vy;
     }
   }
 }
-const init = () => {
+window.init = () => {
   const ctx = document.getElementById('ctx');
   window.stage = new createjs.Stage(ctx);
+  window.arena = new createjs.Container();
+  const background = new createjs.Shape();
+  background.graphics.beginRadialGradientFill(["#777","#444"], [0, 1], WIDTH / 2, HEIGHT / 2, 0,WIDTH / 2, HEIGHT / 2, 1000).drawRect(0, 0, WIDTH, HEIGHT);
+  arena.addChild(background);
+  stage.addChild(arena);
+  arena.setBounds(0, 0, 1000, 600);
   stage.mouseEventsEnabled = true;
   stage.keyboardEventsEnabled = true;
   createjs.Ticker.framerate = 60;
@@ -112,10 +71,17 @@ const init = () => {
 const addGame = () => {
   window.player = new Player(1);
   window.circle = new createjs.Shape();
+  window.findme = new createjs.Shape();
   circle.graphics.beginFill("DeepSkyBlue").drawCircle(0, 0, 25);
-  circle.x = player.x;
-  circle.y = player.y;
+  findme.graphics.beginFill("Orange").drawCircle(0, 0, 25);
+
+  circle.x = 500;
+  circle.y = 300;
+  findme.x = player.x;
+  findme.y = player.y;
+
   stage.addChild(circle);
+  arena.addChild(findme);
   stage.update();
   startGame();
 };
@@ -127,7 +93,7 @@ const startGame = () => {
 
 const update = () => {
   player.updatePostition();
-  circle.x = player.x;
-  circle.y = player.y;
+  stage.setChildIndex(circle, 1)
+
   stage.update();
 };
